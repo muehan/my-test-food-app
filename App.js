34d970de19738af4dbcbd2ddec3230f2fd3355fd
muehan <tdc@gmx.ch>
2024-04-16 19:50:53 +0200

    init update

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Updates from 'expo-updates';
 
 export default function App() {
+  const runTypeMessage = Updates.isEmbeddedLaunch
+  ? 'This app is running from built-in code'
+  : 'This app is running an update';
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>This is the best app ever</Text>
+      <Text>{runTypeMessage}</Text>
+      <Text>update test</Text>
+      <Text>Sali du food shopper</Text>
+      <Text>this is supposed to be here</Text>
       <StatusBar style="auto" />
     </View>
   );
